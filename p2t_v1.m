e0261e9be8a1d0d809a93bf9c0c0660b70cae8de
RobinsonYao <129077021+RobinsonYao@users.noreply.github.com>
2023-03-28 00:24:33 +0800

    Update p2t_v1.m

--- p2t_v1.m
+++ p2t_v1.m
@@ -73,6 +73,19 @@ x_rho = xcosd(x_axis_line.theta) + ysind(x_axis_line.theta);
 y_rho = xcosd(y_axis_line.theta) + ysind(y_axis_line.theta);
 x_axis_dist = abs(x_rho - x_axis_rho);
 y_axis_dist = abs(y_rho - y_axis_rho);
+for i = 1:size(curve_xy,1)
+    x = curve_xy(i,1);
+    y = curve_xy(i,2);
+    x_rho = xcosd(x_axis_line.theta) + ysind(x_axis_line.theta);
+    y_rho = xcosd(y_axis_line.theta) + ysind(y_axis_line.theta);
+    x_axis_dist = abs(x_rho - x_axis_rho);
+    y_axis_dist = abs(y_rho - y_axis_rho);
+    if x_axis_dist <= y_axis_dist
+        x_points = [x_points; [x, x_rho]];
+    else
+        y_points = [y_points; [y_rho, y]];
+    end
+end
 if x_axis_dist <= y_axis_dist
 x_points = [x_points; [x, x_rho]];
 else
