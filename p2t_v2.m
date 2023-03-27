9e96d30d076590929e7f3f58b8cf220fe4e1f6b3
RobinsonYao <129077021+RobinsonYao@users.noreply.github.com>
2023-03-28 01:05:45 +0800

    Update p2t_v2.m

--- p2t_v2.m
+++ p2t_v2.m
@@ -19,6 +19,54 @@ curve_xy = curve_lines.Position;
 
 % Get axis lines
 [x_axis_line, y_axis_line] = get_axis_lines(I);
+function [x_points, y_points] = p2t_v2(filename)
+% P2T_V2 extract data points from image
+%   [x_points, y_points] = P2T_V2(filename) reads in the image specified by
+%   filename, detects the axis lines, and extracts data points from the
+%   image. The x_points and y_points are returned as output.
+
+    % Read in image
+    I = imread(filename);
+
+    % Detect axis lines
+    [x_axis_line, y_axis_line] = detect_axis_lines(I);
+
+    % Define output arrays
+    x_points = [];
+    y_points = [];
+
+    % Extract data points
+    for i = 1:size(curve_lines,2)
+        curve_lines(i).points = get_points_on_line(curve_lines(i).line, I);
+    end
+
+    % Plot axis lines and data points
+    figure;
+    imshow(I);
+    hold on;
+    if ~isempty(x_axis_line)
+        xy = [x_axis_line.point1; x_axis_line.point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+    end
+    if ~isempty(y_axis_line)
+        xy = [y_axis_line.point1; y_axis_line.point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','red');
+    end
+    for i = 1:size(curve_lines,2)
+        if ~isempty(curve_lines(i).points)
+            plot(curve_lines(i).points(:,1), curve_lines(i).points(:,2), 'LineWidth',2,'Color','blue');
+        end
+    end
+    hold off;
+end
+
+function [x_axis_line, y_axis_line] = detect_axis_lines(I)
+    % ... function body here ...
+end
+
+function points = get_points_on_line(line, I)
+    % ... function body here ...
+end
 
 % Find intersection points
 x_points = [];
